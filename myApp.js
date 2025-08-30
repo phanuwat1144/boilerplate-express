bcb7613d48cec1cae7757e4e07b08252137ae81f
YokPhanu <162823222+phanuwat1144@users.noreply.github.com>
2025-08-30 22:14:19 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,10 @@
 let express = require('express');
 let app = express();
 
-// ตัวอย่าง route แรก
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-// เริ่ม server บน port 3000
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
