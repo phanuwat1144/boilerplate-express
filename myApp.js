3d0c4dc4f0f5b80a3482ef02e8a54977b5b5f8c7
YokPhanu <162823222+phanuwat1144@users.noreply.github.com>
2025-08-30 22:21:56 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+let express = require('express');
+let app = express();
+
+// เพิ่ม route ที่ root "/"
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
+
+module.exports = app;
